c21555b00ead1c1a9cddfefeea182d7a22cdd90a
Erick Lopez <ericklopez76@gmail.com>
2014-08-22 15:28:01 -0600

    update

--- src/getMails.py
+++ src/getMails.py
@@ -1,5 +1,7 @@
 __author__ = 'ErickLopez76'
 import imaplib
+import email
+from mailsBackupTools import *
 
 imap_host = 'mail.inclusionsocial.gob.sv'
 imap_user = 'elopez@inclusionsocial.gob.sv'
@@ -17,12 +19,28 @@ imap.select("inbox") #Connected to inbox
 
 print(folderStatus)
 
-result, data = imap.search(None, "ALL")
+result, data = imap.uid('search', None, "ALL")
 
 ids = data[0] #data is a list.
 print(ids)
 id_list = ids.split() #ids is a space separate string
-lastest_email_id = id_list[-1] # get the lastest
+lastest_email_id = data[0].split()[-1] # get the lastest
 
-result, data = imap.fetch(lastest_email_id,"(RFC822)")  #fetch the mail
+#result, data = imap.fetch(lastest_email_id,"(RFC822)")  #fetch the mail
 
+result, data = imap.uid('fetch', lastest_email_id, '(RFC822)')
+
+raw_email = data[0][1]
+
+print(raw_email)
+print (lastest_email_id)
+print(sumar(1,1))
+
+email_message = email.message_from_bytes(raw_email)
+print('To: ')
+print(email_message['To'])
+print('From: ')
+print (email.utils.parseaddr(email_message['From']))
+print('cc: ')
+print(email_message['cc'])
+print (email_message.items()) #print all headers
---
+++ src/mailsBackupTools.py
@@ -0,0 +1,11 @@
+__author__ = 'ErickLopez76'
+import email
+
+def sumar(a,b):
+    """
+    This module is for help
+    :param a: First integer
+    :param b:
+    :return:
+    """
+    return a + b
