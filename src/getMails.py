607b2a1d22a0c0e273e1f8c9025d781eb7f109bd
Erick Lopez <ericklopez76@gmail.com>
2014-08-22 13:40:09 -0600

    update

--- src/getMails.py
+++ src/getMails.py
@@ -11,5 +11,18 @@ imap.login(imap_user, imap_pass)
 
 folderStatus, UnseenInfo = imap.status('INBOX',"(UNSEEN)")
 
+imap.list()
+
+imap.select("inbox") #Connected to inbox
+
 print(folderStatus)
+
+result, data = imap.search(None, "ALL")
+
+ids = data[0] #data is a list.
+print(ids)
+id_list = ids.split() #ids is a space separate string
+lastest_email_id = id_list[-1] # get the lastest
+
+result, data = imap.fetch(lastest_email_id,"(RFC822)")  #fetch the mail
 
